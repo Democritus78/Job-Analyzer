0326cdd9cb01f1355d9150ff563f781e717ffab4
Steven Crowther <stevencrowther@MacBookPro.attlocal.net>
2025-05-14 21:55:08 -0400

    Testing out form submission in Django.

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        print(f"Received Name: {name}")
+        return HttpResponseRedirect('/')
+    
     return render(request, 'myapp/index.html')
