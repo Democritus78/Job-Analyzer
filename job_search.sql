7d3f4d386e5101bafc3d1f62e5e85a4a722015eb
Steven Crowther <stevencrowther@MacBookPro.attlocal.net>
2025-05-23 20:18:42 -0400

    Removed redundant tables in job_search.sql file.
    Added various skill tables to job_search.sql file, then added tables to database file.

--- job_search.sql
+++ job_search.sql
@@ -1,86 +1,89 @@
-create database job_search;
 
-use job_search;
-
-create table programming_language (
+create table if not exists programming_language ( -- Skill
     id                  int primary key,
-    software_id         int not null,
-    name                varchar(255) not null,
+    my_project_id       int not null,
+    name                text check (length(name) <= 255) not null,
 
-    foreign key (my_profile_id) references my_profile(id)
+    foreign key (my_project_id) references my_project(id)
 ) strict;
 
-create table software (
+-- Framework, Software, or Library used.
+create table if not exists framework ( -- Skill
     id                  int primary key,
-    my_profile_id       int not null,
-    name                varchar(255) not null
+    my_project_id       int not null,
+    name                text check (length(name) <= 255) not null,
 
-    foreign key (my_profile_id) references my_profile(id)
+    foreign key (my_project_id) references my_project(id)
 ) strict;
 
-create table my_project (
+create table if not exists tools ( -- Skill
+    id                  int primary key,
+    my_project_id       int not null,
+    name                text check (length(name) <= 255) not null,
+
+    foreign key (my_project_id) references my_project(id)
+) strict;
+
+create table if not exists databases ( -- Skill
+    id                  int primary key,
+    my_project_id       int not null,
+    name                text check (length(name) <= 255) not null,
+
+    foreign key (my_project_id) references my_project(id)
+) strict;
+
+create table if not exists cloud_service ( -- Skill
+    id                  int primary key,
+    my_project_id       int not null,
+    name                text check (length(name) <= 255) not null,
+
+    foreign key (my_project_id) references my_project(id)
+) strict;
+
+create table if not exists my_project (
     id                          int primary key,
-    programming_language_id     int not null,
-    name                        varchar(255) not null,
-    description                 varchar(255) not null,
-    date_created                current_timestamp not null,
-    date_completed              current_timestamp,
-    status                      varchar(255) not null,
-
-    foreign key (programming_language_id) references programming_language(id)
+    name                        text check (length(name) <= 255) not null,
+    description                 text check (length(description) <= 255) not null,
+    date_created                text default current_timestamp not null,
+    date_completed              text default current_timestamp,
+    status                      text not null
 ) strict;
 
-create table job_offer (
+create table if not exists job_offer (
     id                      int primary key,
-    company                 varchar(255) not null,
+    company                 text check (length(company) <= 255) not null,
     job_id                  int,
-    position                varchar(255) not null,
-    location                varchar(255) not null,
-    description             varchar(255) not null,
-    salary                  decimal(10, 2),    
-    compensation_range      varchar(255),
-    date_posted             current_timestamp,
-    date_applied            current_timestamp not null,
-    application_status      varchar(1) not null,
+    position                text check (length(position) <= 255) not null,
+    location                text check (length(location) <= 255) not null,
+    description             text check (length(description) <= 255) not null,
+    salary                  text,    
+    compensation_range      text check (length(compensation_range) <= 255),
+    date_posted             text default current_timestamp,
+    date_applied            text default current_timestamp not null,
+    application_status      text not null
 ) strict;
 
-create table job_responsibility (
+create table if not exists job_responsibility (
     id                  int primary key,
     job_offer_id        int not null,
-    responsibility      varchar(255) not null,
+    responsibility      text check (length(responsibility) <= 255) not null,
 
     foreign key (job_offer_id) references job_offer(id)
 ) strict;
 
-create table job_requirement (
+create table if not exists job_requirement (
     id                  int primary key,
     job_offer_id        int not null,
-    type_t              varchar(255),
-    requirement         varchar(255) not null,
+    type_t              text check (length(type_t) <= 255),
+    requirement         text check (length(requirement) <= 255) not null,
 
     foreign key (job_offer_id) references job_offer(id)
 ) strict;
 
-create table job_benefit (
+create table if not exists job_benefit (
     id                  int primary key,
     job_offer_id        int not null,
-    benefit             varchar(255) not null,
-
-    foreign key (job_offer_id) references job_offer(id)
-) strict;
-
-create table job_programming_language (
-    id                  int primary key,
-    job_offer_id        int not null,
-    language            varchar(255) not null,
-
-    foreign key (job_offer_id) references job_offer(id)
-) strict;
-
-create table job_software (
-    id                  int primary key,
-    job_offer_id        int not null,
-    name                varchar(255) not null,
+    benefit             text check (length(benefit) <= 255) not null,
 
     foreign key (job_offer_id) references job_offer(id)
 ) strict;
