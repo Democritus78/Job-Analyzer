6b7dcf2e217b86d86d04c5a7004562195dcdf685
Steven Crowther <stevencrowther@Stevens-MacBook-Pro.local>
2025-11-12 18:00:50 -0500

    Modified view.py's index method to extract job offer details when 'Add Job Offer' submission button is clicked.

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
+import re
 import sqlite3
 
 from django.shortcuts import render
@@ -36,8 +37,119 @@ def fit_score(job_offer_skill_ids: list[int]) -> int:
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        print(f"Received Name: {name}")
+        #name = request.POST.get('name')
+        #print(f"Received Name: {name}")
+        action = request.POST.get('action')
+        #print(action)
+        
+        if action == 'add_job_offer':  
+            company_name = request.POST.get('company_name')
+            '''
+            print(f'company name: {company_name}')
+            print('\n')
+            '''
+                        
+            job_id = request.POST.get('job_id')
+            '''
+            print(f'job id: {job_id}')
+            print('\n')
+            '''
+            
+            position = request.POST.get('position')
+            '''
+            print(f'position: {position}')
+            print('\n')
+            '''
+            
+            # Extracts information about the job
+            about_the_job = request.POST.get('about_the_job')
+            if len(about_the_job) != 3:
+                about_the_job_details = about_the_job.split('\n')
+                about_the_job_details = [' '.join(about_the_job_detail.split()[1:]) for about_the_job_detail in about_the_job_details]
+                '''
+                print('about the job:')
+                for about_the_job_detail in about_the_job_details:
+                    print(f'\t{about_the_job_detail}')
+                print('\n')
+                '''
+            else:
+                print(f'about the job: {about_the_job}')
+            
+            location = request.POST.get('location')
+            '''
+            print(f'location: {location}')
+            print('\n')
+            '''
+            
+            salary = request.POST.get('salary')
+            '''
+            print(f'salary: {salary}')
+            print('\n')
+            '''
+
+            # Extracts requirements   
+            responsibilities = request.POST.get('responsibilities').split('\n')
+            responsibilities = [' '.join(responsibility.split()[1:]) for responsibility in responsibilities]
+            '''
+            print('responsibilities:')
+            for responsibility in responsibilities:
+                print(f'\t{responsibility}')
+            print('\n')
+            '''
+            
+            # Extracts requirements    
+            requirements = request.POST.get('requirements').split('\n')
+            requirements = [' '.join(requirement.split()[1:]) for requirement in requirements]
+            lowercase_requirements = [requirement.lower() for requirement in requirements]
+            conn = sqlite3.connect('/Users/stevencrowther/Documents/Coding/web development/job_search-root/db.sqlite3')
+            cursor = conn.cursor()
+            cursor.execute('select id, lower(name) from skill')
+            results = cursor.fetchall()
+            #print(results)
+            
+            job_offer_skill_ids = set()
+            print(f'Job Offer Skills:')
+            for lowercase_requirement in lowercase_requirements:
+                print(f'\t{lowercase_requirement}')
+                for id, name in results:
+                    #print(f'\tskill name: {name}')
+                    '''
+                    if name == 'c++':
+                        if re.search(r'(?<!\w)c\+\+(?!\w)', lowercase_requirement):
+                            job_offer_skill_ids.add(id)
+                            print(f'\tskill found:{name}')
+                    elif name == 'c#':
+                        if re.search(r'(?<!\w)c#(?!\w)', lowercase_requirement):
+                            job_offer_skill_ids.add(id)
+                            print(f'\tskill found:{name}')
+                    elif name == 'f#':
+                        if re.search(r'(?<!\w)f#(?!\w)', lowercase_requirement):
+                            job_offer_skill_ids.add(id)
+                            print(f'\tskill found:{name}')
+                    else:
+                    '''
+                    if re.search(rf'(?<!\w){re.escape(name)}(?!\w)', lowercase_requirement):
+                        if id not in job_offer_skill_ids:
+                            job_offer_skill_ids.add(id)
+                            print(f'\tskill found: {name}')
+                        
+            benefits = request.POST.get('benefits')
+            if len(benefits) != 3:
+                benefits = benefits.split('\n')
+                benefits = [' '.join(benefit.split()[1:]) for benefit in benefits]
+                '''
+                print('benefits:')
+                print(f'\n{benefits}')
+                print('\n')
+                '''
+            else:
+                print(f'\t{benefits}')
+            
+        elif action == 'update_job_offer':
+            pass
+        elif action == 'cancel_job_offer':
+            pass
+        
         return HttpResponseRedirect('/')
     
     return render(request, 'myapp/index.html')
